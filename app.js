ee1e645ee4d1a5fce77ba02ec85db3a98b97a242
harelsapir2009-maker <harel.sapir2009@gmail.com>
2026-02-05 01:48:42 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
 function testButton(){
     alert("JavaScript is working ✅");
 }
+let availableMachines = [];
+
+function scanGym() {
+  const input = document.createElement("input");
+  input.type = "file";
+  input.accept = "image/*";
+  input.capture = "environment";
+
+  input.onchange = () => {
+    alert("Gym photo saved ✅\n(בעתיד זה יזהה אוטומטית את המכשירים)");
+    
+    // בינתיים נוסיף מכשירים ידנית לדוגמה
+    availableMachines = [
+      "Bench Press",
+      "Lat Pulldown",
+      "Leg Press",
+      "Cable Machine",
+      "Dumbbells"
+    ];
+
+    localStorage.setItem("machines", JSON.stringify(availableMachines));
+  };
+
+  input.click();
+}
